4ce9247e08eae9e5b0567e77d451307eb2233732
manoj97738 <manoj97738@gmail.com>
2023-04-23 12:24:11 +0530

    Form validation

--- front-end/apna-adda/src/app/myhllow/myhllow.component.ts
+++ front-end/apna-adda/src/app/myhllow/myhllow.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sit-myhllow',
@@ -9,19 +9,37 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class MyhelloComponent implements OnInit {
   public signinForm: FormGroup;
   public something: FormControl = new FormControl("");
-  ngmd:any;
+  ngmd: any;
   constructor() {
     this.signinForm = new FormGroup({
-      email: new FormControl(""),
-      password: new FormControl("", []),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.maxLength(10), Validators.minLength(9),]),
     })
   }
   mysubmit() {
-    console.log(this.signinForm.value);
+    console.log(this.signinForm);
     console.log(this.something)
     console.log(this.ngmd)
+    if (this.signinForm.valid) {
+      alert("I am valid")
+    }
   }
   ngOnInit(): void {
   }
-
+  iamValid(control: string) {
+    const ctr = this.signinForm.get(control);
+    if (ctr?.errors) {
+      if (ctr.errors["required"]) {
+        return "this field is required";
+      }
+      if (ctr.errors["minlength"]) {
+        return "this field is minlength";
+      }
+      if (ctr.errors["maxlength"]) {
+        return "this field is maxlength";
+      }
+      return "";
+    }
+    return "";
+  }
 }
